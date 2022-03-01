5bc75fe3ae4baf8b7f92492825b27b105a892070
David Gonzalez <david623@revature.net>
2022-03-01 15:16:35 -0500

    SecondSwiftUIView

---
+++ GithubDemo-Video-1/SecondSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondSwiftUIView.swift
+//  GithubDemo-Video-1
+//
+//  Created by David Gonzalez on 3/1/22.
+//
+
+import SwiftUI
+
+struct SecondSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondSwiftUIView()
+    }
+}
